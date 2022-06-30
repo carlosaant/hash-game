f7e751319eaa1cbe4a24f216b1048d2ca53db857
Antonio Carlos <antc.minga@gmail.com>
2022-06-30 16:07:24 -0300

    feat: criada logica para determinar vencedor

--- js/game.js
+++ js/game.js
@@ -2,6 +2,17 @@
 
 let board = ['', '', '', '', '', '', '', '', ''];
 
+const winState = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 let p1 = {
   ativo: true,
   symbol: 'x'
@@ -11,16 +22,48 @@ let p2 = {
   symbol: 'o'
 };
 
+let gameOver = false;
+
+// ---------------------------------
+
 function handleMove(position) {
+  if (gameOver) {
+    return;
+  }
+
   if (board[position] == '') {
     if (p1.ativo) {
       board[position] = p1.symbol;
-      p1.ativo = false;
-      p2.ativo = true;
+      gameOver = isWin();
+      if (!gameOver) {
+        p1.ativo = false;
+        p2.ativo = true;
+      }
     } else if (p2.ativo) {
       board[position] = p2.symbol;
-      p2.ativo = false;
-      p1.ativo = true;
+      gameOver = isWin();
+      if (!gameOver) {
+        p2.ativo = false;
+        p1.ativo = true;
+      }
     }
   }
+}
+
+function isWin() {
+  for (let index = 0; index < winState.length; index++) {
+    const sequencia_elements = winState[index];
+    const pos1 = sequencia_elements[0];
+    const pos2 = sequencia_elements[1];
+    const pos3 = sequencia_elements[2];
+
+    if (
+      board[pos1] == board[pos2] &&
+      board[pos1] == board[pos3] &&
+      board[pos1] != ''
+    ) {
+      return true;
+    }
+  }
+  return false;
 }
